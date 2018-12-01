39070e7546bf81e4937c263d41c29d1e65924a78
Pixeldenker <bram@pixeldenker.nl>
2018-12-01 13:02:09 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    hot: true
+  },
+  resolveLoader: {
+    modules: ['node_modules', path.resolve(__dirname, 'loaders')],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+            'css-loader',
+            'dutch-css-loader'
+        ]
+      }
+    ]
+  }
+};
