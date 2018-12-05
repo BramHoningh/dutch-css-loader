a61320ed34588285db82de7ae4f2aa818df30091
bram.honingh <bram.honingh@mediamonks.com>
2018-12-05 11:56:59 +0100

    Add nodemon to workflow

--- loaders/dutch-css-loader.js
+++ loaders/dutch-css-loader.js
@@ -2,10 +2,21 @@ module.exports = function(content, map, meta) {
   const returnType = '.title{background-color:red;}';
 
   const parsedContent = parseContent(content);
+  console.log('========== PARSED CONTENT ==========');
+  console.log(parsedContent);
+  console.log('======== END PARSED CONTENT ===========');
 
   this.callback(null, returnType, map, meta);
 };
 
 function parseContent(content) {
-  console.log('hallo');
+  if (!content.length) return '';
+
+  const pattern = '((\\s*?(?:\\/\\*[\\s\\S]*?\\*\\/)?\\s*?@media[\\s\\S]*?){([\\s\\S]*?)}\\s*?})|(([\\s\\S]*?){([\\s\\S]*?)})';
+  const unified = new RegExp(pattern, 'gi');
+  let arr;
+
+  console.log('arr', arr);
+
 }
+
